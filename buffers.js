f51395335372fe0451a73f3d6fd0d738e09af82a
pl <0x00.pl@gmail.com>
2013-05-11 01:52:00 +0800

    add input.

--- buffers.js
+++ buffers.js
@@ -20,6 +20,12 @@ var buffers={
 		    pos.data= min(sender.ref('text').data.length,pos.data+1);
 		}
 	    }),
+	    '.user_input':new file_system.file_bloc(function(sender,txt){
+		var pos= sender.ref('pos-cur').data;
+		var old_txt= sender.ref('text').data;
+		sender.ref('text').data= old_txt.substring(0,pos)+txt+old_txt.substring(pos);
+		sender.ref('pos-cur').data+= txt.length;
+	    }),
 	});
 	root.ref('data').ref('buffers').push('hello',tr);
 	return tr;
@@ -34,10 +40,15 @@ var buffers={
 		var cur= root.ref('data').ref('buffer-cur').data;
 		return bfs.ref(cur).ref('.user_keydown').data(bfs.ref(cur),key_event);
 	    }),
+	    '.user_input':new file_system.file_bloc(function(bfs,txt){
+		var cur= root.ref('data').ref('buffer-cur').data;
+		return bfs.ref(cur).ref('.user_input').data(bfs.ref(cur),txt);
+	    }),
 	}));
 	root.ref('data').push('buffer-cur',new file_system.file_bloc("hello"));
 	event.register('user_keydown', root.ref('data').ref('buffers').ref('.user_keydown').data);
-   
+	event.register('user_input', root.ref('data').ref('buffers').ref('.user_input').data);
+      
 	buffers.red_hello_buffer();
     },
 };
